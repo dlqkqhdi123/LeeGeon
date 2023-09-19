3e8207faa8c7f55befca3bcfaffa09c1afcc9226
dlqkqhdi123 <dlrjs6949@naver.com>
2023-09-19 16:18:34 +0900

    click_event

---
+++ 2023-09-19/button_click_event/script.js
@@ -0,0 +1,26 @@
+const title = document.getElementById('title')
+console.log(title)
+
+// 제목을 클릭했을때 alert 창이 뜨면서 '클릭'이라는 문구가 출력
+// addEnentListener('이벤트이름', 실행할 함수)
+title.addEventListener('click', () => {
+    //alert('클릭')
+    //클릭햇을떄 컬러가 red가 되도록 변경 
+    if(title.style.color === 'red'){
+        title.style.color = 'black'
+    } else{
+        title.style.color = 'red'
+    }
+    const button = document.querySelector('#btn')
+    container = document.querySelector('.container')
+    button.addEventListener('click', () => {
+        container.classList.add('bg')
+    })
+        
+    
+})
+
+
+// 버튼 가져와서 변수에담는다
+// 컨테이너 가져와서 변수에담는다 
+// 버튼 클릭햇을때 컨테이너 변수에 비지 클래스를 추가한다
