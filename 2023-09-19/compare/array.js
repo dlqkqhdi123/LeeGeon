43da20d2fa2ead84dc8ef4cdfe579f0d5dc0c229
dlqkqhdi123 <dlrjs6949@naver.com>
2023-09-19 11:20:19 +0900

    array

---
+++ 2023-09-19/compare/array.js
@@ -0,0 +1,16 @@
+// console.log('adsdsd')
+//포켓몬
+const pokemon = ['피카츄','이브이','라이츄','파이리','꼬부기']
+pokemon.push('망나뇽')
+//포켓몬 배열에 이상해씨를 추가해주세요
+pokemon.push('이상해씨')
+// 배열에 마지막 요소를 제거 pop()
+pokemon.pop()
+console.log(pokemon)
+//배열의 길이를 알아내는 법
+console.log(pokemon.length)
+console.log(pokemon[0])// 피카츄
+
+//파이리를 출력해주세요 
+console.log(pokemon[3]) //파이리
+
