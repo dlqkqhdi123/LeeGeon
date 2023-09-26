df1efd52d4dbe9a8fb358473a560d64e7a506af6
dlqkqhdi123 <dlrjs6949@naver.com>
2023-09-26 12:36:25 +0900

    forEach 추가

--- 2023-09-26/counter/script.js
+++ 2023-09-26/counter/script.js
@@ -3,23 +3,36 @@ const dataList = document.querySelectorAll('[data-counter]')
 // console.log(timer)
 console.log(dataList)
 
-for(let i = 0; i < dataList.length; i++){
-    const target = dataList[i].dataset.counter
-    const timerText = dataList[i].querySelector('.timer')
-    const step = Math.floor(target / 600)
-    console.log(step)
+// for(let i = 0; i < dataList.length; i++){
+//     const target = dataList[i].dataset.counter
+//     const timerText = dataList[i].querySelector('.timer')
+//     const step = Math.floor(target / 600)
+//     console.log(step)
+//     let index = 0;
+//     setInterval(() => {
+//         index++
+//         // console.log(index)
+//         if(target >= index){
+//             index = index + step
+//             timerText.textContent = index
+//             console.log(index)
+//         }
+//     }, 10)
+// }
+
+dataList.forEach(item => {
     let index = 0;
+    const target = item.dataset.counter
+    const timerText = item.querySelector('.timer')
+    const step = Math.floor(target / 200)
     setInterval(() => {
-        index++
-        // console.log(index)
-        if(target >= index){
+        if(index < target){
             index = index + step
             timerText.textContent = index
-            console.log(index)
         }
-    }, 10)
-}
+    })
+}, 1)
 
 /**
- *   step 3개 도착이 같아지게끔 만듦 
+ *   step n개 도착이 같아지게끔 만듦 
  */
