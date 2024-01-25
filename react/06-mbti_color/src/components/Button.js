a28b923eef12bec7d8d10c7ccdc197ce21be07f9
dlqkqhdi123 <dlrjs6949@naver.com>
2024-01-25 17:30:19 +0900

    123

--- react/06-mbti_color/src/components/Button.js
+++
@@ -1,6 +0,0 @@
-import styles from "./Button.module.css";
-
-export default function Button({ className, children }) {
-  const classNames = `${styles.button} ${className}`;
-  return <button className={classNames}>{children}</button>;
-}
