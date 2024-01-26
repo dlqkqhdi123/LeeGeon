c3b8750da94f15a9c3608ff84d8c5e48ccaba897
dlqkqhdi123 <dlrjs6949@naver.com>
2024-01-26 17:12:38 +0900

    김경숙님꺼 추가

---
+++ 김경숙/src/mypages/MyPage.js
@@ -0,0 +1,23 @@
+import SideBar from "./SideBar.js";
+import { Outlet } from "react-router-dom";
+import styles from "./MyPage.module.css";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #f8ebd8;
+  display: flex;
+`;
+
+function MyPage() {
+  return (
+    <>
+      <SideBar />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
+  );
+}
+
+export default MyPage;
